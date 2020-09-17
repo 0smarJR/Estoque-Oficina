2356af097911da32e8cc16a50f50838f36bfb65f
0smarJR <osmar.emidio.junior@gmail.com>
2020-09-17 15:29:22 -0300

    Arrumando Tabelas

--- estoque/app/admin.py
+++ estoque/app/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Estoque, EstoqueItens
 # Register your models here.
+
 @admin.register(Produto)
 class ProdutosAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome', 'quantidade']
+    list_display = ['id', 'nome', 'marca', 'quantidade', 'preco']
+
+class EstoqueItensInline(admin.TabularInline):
+    model = EstoqueItens
+    extra = 0
+
+@admin.register(Estoque)
+class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInline,)
+    list_display = ['id', 'usuario', 'nf', 'movimento']
+    search_fields = ('nf',)
+    list_filter = ('usuario',)
+    date_hierarchy = 'created'
+
--- estoque/app/models.py
+++ estoque/app/models.py
@@ -4,12 +4,48 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
+    marca = models.CharField(max_length=100, default=None)
     quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=100, decimal_places= 2, default=None)
     ativo = models.BooleanField(default=True)
-    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return str(self.nome)
 
     class Meta:
         db_table = 'produto'
+
+class TimeStampedModel(models.Model):
+    created = models.DateField('criado em', auto_now_add=True, auto_now=False)
+    modified = models.DateField('modificado em', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+
+MOVIMENTO = (
+    ('e', 'entrada'),
+    ('s', 'saida')
+)
+
+class Estoque(TimeStampedModel):
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    nf = models.PositiveIntegerField('nota fiscal', null=True, blank=True)
+    movimento = models.CharField(max_length=1, choices=MOVIMENTO)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return str(self.pk)
+
+class EstoqueItens(models.Model):
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.PositiveIntegerField()
+    saldo = models.PositiveIntegerField()
+
+    class Meta:
+        ordering=('pk',)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.pk, self.estoque.pk,self.produto.pk)
