7415d7a7233a28619d793ad9f9f914c41b1d18f0
0smarJR <osmar.emidio.junior@gmail.com>
2020-09-17 16:02:26 -0300

    Povoando db

--- estoque/app/models.py
+++ estoque/app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
+    codigo = models.IntegerField(default=None)
     marca = models.CharField(max_length=100, default=None)
     quantidade = models.IntegerField()
     preco = models.DecimalField(max_digits=100, decimal_places= 2, default=None)
