20a13aa5c2bda4a08ee0d6b2ce9c64cf30c01710
0smarJR <osmar.emidio.junior@gmail.com>
2020-11-16 18:00:34 -0300

    Add requirements.txt & Procfile

---
+++ estoque/app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Produto, Estoque, EstoqueItens
+
+class ProdutoForm(forms.ModelForm):
+
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+class EstoqueForm(forms.ModelForm):
+
+    class Meta:
+        model = Estoque
+        fields = '__all__'
+
+class EstoqueItensForm(forms.ModelForm):
+
+    class Meta:
+        model = EstoqueItens
+        fields = '__all__'
